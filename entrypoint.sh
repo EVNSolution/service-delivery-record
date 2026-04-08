16c7938208a82de78bf786f20fd31d4be2db07cc
OziinG <dlajiin@gmail.com>
2026-04-08 15:38:46 +0900

    feat: add image build pipeline for delivery record

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -eu
 
+if [ "$#" -gt 0 ]; then
+  exec "$@"
+fi
+
 python manage.py migrate --noinput
-exec "$@"
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers "${GUNICORN_WORKERS:-2}" \
+  --timeout "${GUNICORN_TIMEOUT:-60}"
